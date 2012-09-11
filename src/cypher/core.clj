b2c6e7256fd74dc563af6749108cca3fc6181705
David James Humphreys <davidjameshumphreys@gmail.com>
2012-09-11 20:57:26 +0100

    Added the functionality to deal with uppercase and other characters.

--- src/cypher/core.clj
+++ src/cypher/core.clj
@@ -3,15 +3,14 @@
 (defn rot13
   "Encrypt a message"
   [message]
- 	(reduce str (map rotchar message))
-  )
+ 	(reduce str (map rotchar message)))
 
 (defn rotchar
   "rotate char"
   [c]
-  (let [offset (- (int c) (int \a))]
-    (if (neg? offset)
-      c
-      (first (drop (+ 13 offset) (cycle (seq "abcdefghijklmnopqrstuvwxyz")))))))
-
-()
+	(cond
+     	(Character/isUpperCase c)
+     		(first (drop (+ 13 (- (int c) (int \A))) (cycle (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))))
+    	(Character/isLowerCase c)
+     		(first (drop (+ 13 (- (int c) (int \a))) (cycle (seq "abcdefghijklmnopqrstuvwxyz"))))
+      	:else c))

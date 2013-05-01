c98d965dbab87a41d094cecf748c4b4cf79e1258
David James Humphreys <davidjameshumphreys@gmail.com>
2013-05-01 08:29:55 +0100

    Vigenère Cypher working.

--- src/cypher/core.clj
+++ src/cypher/core.clj
@@ -15,3 +15,82 @@
   "Encrypt a message"
   [message]
  	(reduce str (map rotchar message)))
+
+
+(defn rot-alpha [shifter]
+  (let [diff (- (int shifter) (int \A))
+        alphabet (cycle (map char (range (int \A) (inc (int \Z)))))]
+    (take 26 (drop diff alphabet))
+    )
+  )
+
+(defn shift-char [shift-amount chr]
+  (let [alphabet (cycle (map char (range (int \A) (inc (int \Z)))))]
+    (first (drop shift-amount alphabet))
+    )
+  )
+
+(defn shift-alphabet [c]
+  (let [alphabet (cycle (map char (range (int \A) (inc (int \Z)))))
+        delta    (dec (- (int c) (int \A)))]
+    (vec (take 26 (drop delta alphabet)))
+    )
+  )
+
+(defn apply-alphabet [a str]
+  (map (fn [i] (get a (inc (- (int i) (int \A))))) str)
+  )
+
+(defn reverse-alphabet [c str]
+  (let [alphabet (cycle (map char (range (int \A) (inc (int \Z)))))
+        delta    (- 26 (- (int c) (int \A)))]
+    (vec (take 26 (drop delta alphabet)))
+    )
+  )
+
+(defn reverse-alphabets [as str]
+  (let [fns (map (fn [a] (partial reverse-alphabet a)) as)]
+    (map apply-alphabet (cycle fns) str)))
+(defn apply-alphabets [as str]
+  (let [fns (map (fn [a] (partial apply-alphabet a)) as)]
+    (map apply-alphabet (cycle fns) str)
+    )
+  )
+
+;;
+
+(defn position [c]
+  (- (int c) (int \A)))
+
+(def full-alphabet (vec (map char (range (int \A) (inc (int \Z))))))
+
+(defn shift-letter [password letter]
+  (let [p (position password)
+        l (position letter)]
+    (get full-alphabet (rem (+ p l) 26)))
+  )
+(defn unshift-letter [password letter ]
+  (let [p (position password)
+        l (position letter)]
+    (get full-alphabet (rem (+ (- 26 p) l) 26))))
+
+(defn- cycle-shift [text pass f]
+  (apply
+   str
+   (map
+    (fn [a b] (a b))
+    (cycle (map (fn [a] (partial f a)) pass))
+    text)
+   )
+  )
+
+(defn scramble [text pass]
+  (cycle-shift text pass shift-letter))
+
+(defn unscramble [text pass]
+  (cycle-shift text pass unshift-letter))
+
+(defn find-cycles [text]
+  (let [pairs (partition-all 2 1 text)
+        ]
+    (frequencies pairs)))
--- test/cypher/core_test.clj
+++ test/cypher/core_test.clj
@@ -2,6 +2,74 @@
   (:use clojure.test
         cypher.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+#_(deftest shift-alphabet-test
+  (testing "Shifting"
+    (is (= (vec "ZABCDE") (take 6 (shift-alphabet \Z))))
+    ))
+
+#_(deftest apply-alphabet-test
+  (testing "Applying the new alphabet"
+    (is (= (vec "ZZZ") (apply-alphabet (shift-alphabet \Z) "AAA")))
+    (is (= (vec "ZAZ") (apply-alphabet (shift-alphabet \Z) "ABA")))
+    ))
+
+#_(deftest multi-test
+  (testing "Cycling two alphabets"
+    (let [pass (map shift-alphabet "ZY")]
+      (is (= (vec "ZYZ") (apply-alphabets pass "AAA")))
+      )
+    ))
+
+#_(deftest two-pass-test
+  (testing "Re-applying the password will provide the original text"
+    (let [password "QWIJIBO"
+          phrase   "IAMAROBOTFROMMARS"
+          crypttext (apply-alphabets password phrase)]
+      (is (= (vec phrase) (reverse-alphabets password crypttext)))
+      )
+    ))
+
+#_(deftest unshift-test
+  (testing "Unshifting an alphabet:"
+    (= (vec "AAA") (reverse-alphabet \Z "ZZZ"))
+    (= (vec "ABA") (reverse-alphabet \Z "ZAZ"))
+    ))
+
+#_(deftest unshift-cycle-test
+  (testing "Unshifting with a password"))
+
+(deftest positions
+  (testing "the indexing of chars:"
+    (are [orig idx] (= orig idx)
+         0  (position \A)
+         25 (position \Z)
+         1  (position \B)
+         24 (position \Y)
+         )
+    ))
+
+(deftest shift-test
+  (testing "Expected shifting of chars:"
+    (are [orig expected pass] (= expected (shift-letter pass orig))
+         \A \A \A
+         \A \B \B
+         )
+))
+
+(deftest unshift-test
+  (testing "Unshifting of chars:"
+    (are [orig expected pass] (= expected (unshift-letter pass orig))
+         \A \A \A
+         \B \B \A
+         \Q \C \O
+         )))
+
+(deftest wiki-test
+  (testing "example from wiki:"
+    (let [txt "ATTACKATDAWN"
+          key "LEMON"
+          exp "LXFOPVEFRNHR"]
+      (is (= exp (scramble txt key)))
+      (is (= txt (unscramble exp key)))
+      )
+    ))
